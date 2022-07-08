db32280223e01a006c2875edd44f7d9de7dc1842
kemianbd <feldmanj@gmail.com>
2022-07-07 22:38:01 -0400

    Turning on the real deal

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-# echo git rev-list --left-right --count  origin/main...feature-branch | awk '{print "Behind "$1" - Ahead "$2""}'
-echo "Branch $2"
+echo git rev-list --left-right --count  origin/main...$2 | awk '{print "Behind "$1" - Ahead "$2""}'
 time=$(date)
 echo "::set-output name=time::$time"
