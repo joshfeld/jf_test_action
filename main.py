eecd54651cfce713063c76eea22c6a7a4a4254dd
kemianbd <feldmanj@gmail.com>
2022-07-08 10:47:00 -0400

    Split by tab

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     print(os.environ["INPUT_BRANCH-NAME"])
-    behind = os.environ["INPUT_COMMIT-DIFF"].split(' ')
+    behind = os.environ["INPUT_COMMIT-DIFF"].split("\t")
     print(behind)
 
 
