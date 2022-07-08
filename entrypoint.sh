51d63c5c9aee87182c7fcb17b4bff0223f3a38fd
kemianbd <feldmanj@gmail.com>
2022-07-08 08:20:11 -0400

    Simplify shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-DIFF=$(git for-each-ref --format="%(push:track)" refs/heads/$(git rev-parse --abbrev-ref HEAD))
-echo $DIFF
-# echo git rev-list --left-right --count  origin/main...$2 | awk '{print "Behind "$1" - Ahead "$2""}'
+echo "Commits: $1"
+echo "Branch: $2"
 time=$(date)
 echo "::set-output name=time::$time"
