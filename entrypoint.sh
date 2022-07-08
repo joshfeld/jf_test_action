0c409a44920318083094bcbb9a6249f72b732ce4
kemianbd <feldmanj@gmail.com>
2022-07-07 22:46:28 -0400

    Update printing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-echo git rev-list --left-right --count  origin/main...$2 | awk '{print "Behind "$1" - Ahead "$2""}'
+DIFF=$(git for-each-ref --format="%(push:track)" refs/heads/$(git rev-parse --abbrev-ref HEAD))
+echo $DIFF
+# echo git rev-list --left-right --count  origin/main...$2 | awk '{print "Behind "$1" - Ahead "$2""}'
 time=$(date)
 echo "::set-output name=time::$time"
