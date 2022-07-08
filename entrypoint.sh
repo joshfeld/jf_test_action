5269806636b6b4788acad49a7448db74de96ce18
kemianbd <feldmanj@gmail.com>
2022-07-07 22:18:59 -0400

    Try adding branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -l
 
 echo "Hello $1"
+# echo git rev-list --left-right --count  origin/main...feature-branch | awk '{print "Behind "$1" - Ahead "$2""}'
+echo "Branch $2"
 time=$(date)
 echo "::set-output name=time::$time"
